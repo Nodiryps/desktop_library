be9bca582aa16f81f207afd6fad382a1cbc70c8a
Zahmidi Othman <zahmidi_oth@hotmail.com>
2019-05-21 13:42:09 +0200

    Début Rental

--- prbd_1819_g19/Rentals.xaml.cs
+++ prbd_1819_g19/Rentals.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,60 @@ namespace prbd_1819_g19
         public Rentals()
         {
             InitializeComponent();
+            DataContext = this;
+            Rentalz = new ObservableCollection<Rental>();
+            Items = new ObservableCollection<RentalItem>();
+            addRentals();
+            //addBook();
+
+
         }
+
+        private ObservableCollection<Rental> rentalz;
+        public ObservableCollection<Rental> Rentalz
+        {
+            get => rentalz;
+            set => SetProperty<ObservableCollection<Rental>>(ref rentalz, value, () => { });
+        }
+
+        private ObservableCollection<RentalItem> items;
+        public ObservableCollection<RentalItem> Items
+        {
+            get => items;
+            set => SetProperty<ObservableCollection<RentalItem>>(ref items, value, () => { });
+        }
+
+        public Rental selectedRental;
+        public Rental SelectedRental
+        {
+            get => selectedRental;
+            set => SetProperty<Rental>(ref selectedRental, value);
+        }
+
+
+        public void addBook()
+        {
+            foreach (var b in SelectedRental.Items)
+            {
+                Items.Add(b);
+
+            }
+        }
+
+        public void addRentals()
+        {
+            foreach (var r in App.Model.Rentals)
+            {
+                Rentalz.Add(r);
+                
+            }
+        }
+
+
+
+
+
+
+
     }
 }
